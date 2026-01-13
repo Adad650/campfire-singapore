831d1bd360577bf6ec48fe59a25b9cf6e4375d1d
techpixel <68567672+techpixel@users.noreply.github.com>
2026-01-13 15:35:05 -0500

    feat: add prisma

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -7,9 +7,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 import node from '@astrojs/node';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), svelte()],
 
   vite: {
     plugins: [tailwindcss()]
@@ -17,5 +19,7 @@ export default defineConfig({
 
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  output: "server"
 });
